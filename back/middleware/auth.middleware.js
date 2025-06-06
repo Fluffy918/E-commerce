35579c13cf7e92706e5a82ddd6a5e1609311b418
Brendan <pidouxbrendan@gmail.com>
2025-06-06 13:14:02 +0200

    middleware

---
+++ back/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+import { config } from 'dotenv';
+
+exports.verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+
+    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ message: 'Token invalide ou expiré' })
+        req.user = { id: decoded.id, role: decoded.role };
+        next();
+    })
+} 
+
+exports.isAdmin = (req, res, next) => {
+    if (!req.user || req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Accès refusé, rôle admin requis'})
+    }
+    next();
+}
